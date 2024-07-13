4fa33d3cb41cd50b5839dd37342439ed75afce22
Enor Cerna <u17200479@utp.edu.pe>
2024-07-13 05:08:08 -0500

    change color

--- components/Canva.tsx
+++ components/Canva.tsx
@@ -3,7 +3,7 @@ import { canvaURL } from "../constants/global.ts";
 function Canva() {
   return (
     <a
-      className="px-3 py-1 bg-green-600 text-white font-semibold rounded-lg"
+      className="px-3 py-1 border border-green-600 hover:bg-green-600 text-white font-semibold rounded-lg"
       href={canvaURL}
       target="_blank"
     >
--- routes/index.tsx
+++ routes/index.tsx
@@ -3,6 +3,8 @@ import Canva from "../components/Canva.tsx";
 export default function Home() {
   return (
     <main class="flex justify-center items-center min-h-screen h-full">
+      <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#1bce7176_100%)]">
+      </div>
       <Canva />
     </main>
   );

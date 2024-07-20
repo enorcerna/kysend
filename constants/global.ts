e7764df1a80db52c2b517f83d58b87be37699659
Enor Cerna <u17200479@utp.edu.pe>
2024-07-19 22:50:55 -0500

    new canva link final

--- constants/global.ts
+++ constants/global.ts
@@ -1,3 +1,3 @@
 const canvaURL =
-  "https://www.canva.com/design/DAGKBkP-Puo/jfgDmEwRCWfVfLexsI4Pjw/edit?utm_content=DAGKBkP-Puo&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton";
+  "https://www.canva.com/design/DAGKr8GmCPY/BXSVHiqhPXr7t3S7ttF2xQ/edit?utm_content=DAGKr8GmCPY&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton";
 export { canvaURL };
